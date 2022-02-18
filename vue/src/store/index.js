3706d4e5534b65fbb137a429c4d9c3e3e9f3d929
deardeadcat <deardeadcat@gmail.com>
2022-02-18 13:53:42 +0200

    created rep

---
+++ vue/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+	state: {
+		user: { name: 'Igor', token: null }
+	},
+	getters: {},
+	actions: {},
+	mutations: {},
+	modules: {}
+});
+
+export default store;
